a1d5e6eca612a19f0547776daab88ac49debc958
이하성 <tedsoftj1123@gmail.com>
2023-12-14 23:25:51 +0900

    feat: 조회

--- src/main/kotlin/com/example/yuseong_allowancepayments_be/domain/allowance/presentation/AllowanceController.kt
+++ src/main/kotlin/com/example/yuseong_allowancepayments_be/domain/allowance/presentation/AllowanceController.kt
@@ -1,18 +1,12 @@
 package com.example.yuseong_allowancepayments_be.domain.allowance.presentation
 
 import com.example.yuseong_allowancepayments_be.domain.allowance.persistence.enums.AllowanceType
-import com.example.yuseong_allowancepayments_be.domain.allowance.presentation.dto.GetAllowanceResponse
 import com.example.yuseong_allowancepayments_be.domain.allowance.presentation.dto.UpdateCashPaymentStatusRequest
 import com.example.yuseong_allowancepayments_be.domain.allowance.presentation.dto.UpdateNewcomerRequest
 import com.example.yuseong_allowancepayments_be.domain.allowance.presentation.dto.UpdatePaymentStoppedRequest
 import com.example.yuseong_allowancepayments_be.domain.allowance.presentation.dto.UpdatePaymentTargetRequest
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.ExportAllowanceInfoService
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.GetAllowanceService
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.SaveAllowanceInfoService
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.UpdateCashPaymentStatusService
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.UpdateNewcomerService
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.UpdatePaymentStoppedService
-import com.example.yuseong_allowancepayments_be.domain.allowance.service.UpdatePaymentTargetService
+import com.example.yuseong_allowancepayments_be.domain.allowance.service.*
+import com.example.yuseong_allowancepayments_be.domain.exel.dto.ParseExelResponse
 import org.jetbrains.annotations.NotNull
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -49,27 +43,27 @@ class AllowanceController(
     @GetMapping
     fun getAllowanceList(
         @RequestParam type: AllowanceType,
-    ): List<GetAllowanceResponse> {
+    ): ParseExelResponse {
         return getAllowanceService.execute(type)
     }
 
     @PatchMapping("/newcomer/{id}")
-    fun updateNewcomer(@PathVariable("id")id: Long, @RequestBody request: UpdateNewcomerRequest) {
+    fun updateNewcomer(@PathVariable("id") id: Long, @RequestBody request: UpdateNewcomerRequest) {
         updateNewcomerService.execute(id, request)
     }
 
     @PatchMapping("/cash/{id}")
-    fun updateNewcomer(@PathVariable("id")id: Long, @RequestBody request: UpdateCashPaymentStatusRequest) {
+    fun updateNewcomer(@PathVariable("id") id: Long, @RequestBody request: UpdateCashPaymentStatusRequest) {
         updateCashPaymentStatusService.execute(id, request)
     }
 
     @PatchMapping("/target/{id}")
-    fun updateTarget(@PathVariable("id")id: Long, @RequestBody request: UpdatePaymentTargetRequest) {
+    fun updateTarget(@PathVariable("id") id: Long, @RequestBody request: UpdatePaymentTargetRequest) {
         updatePaymentTargetService.execute(id, request)
     }
 
     @PatchMapping("/stopped/{id}")
-    fun updateNewcomer(@PathVariable("id")id: Long, @RequestBody request: UpdatePaymentStoppedRequest) {
+    fun updateNewcomer(@PathVariable("id") id: Long, @RequestBody request: UpdatePaymentStoppedRequest) {
         updatePaymentStoppedService.execute(id, request)
     }
 }
--- src/main/kotlin/com/example/yuseong_allowancepayments_be/domain/allowance/service/GetAllowanceService.kt
+++ src/main/kotlin/com/example/yuseong_allowancepayments_be/domain/allowance/service/GetAllowanceService.kt
@@ -2,32 +2,34 @@ package com.example.yuseong_allowancepayments_be.domain.allowance.service
 
 import com.example.yuseong_allowancepayments_be.domain.allowance.persistence.enums.AllowanceType
 import com.example.yuseong_allowancepayments_be.domain.allowance.persistence.repository.CashPaymentStatusJpaRepository
-import com.example.yuseong_allowancepayments_be.domain.allowance.presentation.dto.GetAllowanceResponse
+import com.example.yuseong_allowancepayments_be.domain.allowance.persistence.repository.NewcomerJpaRepository
+import com.example.yuseong_allowancepayments_be.domain.allowance.persistence.repository.PaymentStoppedJpaRepository
+import com.example.yuseong_allowancepayments_be.domain.allowance.persistence.repository.PaymentTargetJpaRepository
+import com.example.yuseong_allowancepayments_be.domain.exel.dto.ParseExelResponse
 import org.springframework.stereotype.Service
 
 @Service
 class GetAllowanceService(
     private val cashPaymentStatusJpaRepository: CashPaymentStatusJpaRepository,
+    private val paymentTargetJpaRepository: PaymentTargetJpaRepository,
+    private val newcomerJpaRepository: NewcomerJpaRepository,
+    private val paymentStoppedJpaRepository: PaymentStoppedJpaRepository
 ) {
-    fun execute(type: AllowanceType): List<GetAllowanceResponse> {
-        val cashPaymentStatusList =
-            cashPaymentStatusJpaRepository.findByAllowanceTypeOrderBySerialNumberAsc(type)
+    fun execute(type: AllowanceType): ParseExelResponse {
+        val paymentTargets = paymentTargetJpaRepository
+            .findByAllowanceTypeOrderBySerialNumberAsc(type)
+        val cashPaymentStatusList = cashPaymentStatusJpaRepository
+            .findByAllowanceTypeOrderBySerialNumberAsc(type)
+        val newcomers = newcomerJpaRepository
+            .findByAllowanceTypeOrderBySerialNumberAsc(type)
+        val paymentStoppedList = paymentStoppedJpaRepository
+            .findByAllowanceTypeOrderBySerialNumberAsc(type)
 
-        return cashPaymentStatusList.stream().map { it ->
-            GetAllowanceResponse(
-                id = it.id!!,
-                serialNumber = it.serialNumber,
-                haengJeongDong = it.hangJungDong,
-                veteransNumber = it.veteransNumber,
-                residentRegistrationNumber = it.residentRegistrationNumber,
-                name = it.name,
-                address = it.address,
-                depositType = it.depositType,
-                sibi = it.sibi,
-                gubi = it.gubi,
-                note = it.note,
-                allowanceType = it.allowanceType,
-            )
-        }.toList()
+        return ParseExelResponse(
+            paymentTargetTab = paymentTargets.map { it.toResponse() },
+            cashPaymentTab = cashPaymentStatusList.map { it.toResponse() },
+            newComerTab = newcomers.map { it.toResponse() },
+            paymentStoppedTab = paymentStoppedList.map { it.toResponse() }
+        )
     }
 }
